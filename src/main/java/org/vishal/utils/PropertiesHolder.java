15da409bd02a4970f23bbe5f2d5435db54c71a34
vishalsugumar <vishalsugumar@outlook.com>
2022-05-12 23:13:24 +0530

    Added page objects and utilities

---
+++ src/main/java/org/vishal/utils/PropertiesHolder.java
@@ -0,0 +1,17 @@
+package org.vishal.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:App.properties")
+public class PropertiesHolder {
+
+    @Value("${app.url}")
+    private String appUrl;
+
+    public String getAppUrl(){
+        return appUrl;
+    }
+}
